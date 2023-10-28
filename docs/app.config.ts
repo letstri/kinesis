a3b4048622cb1b2a41f52a36729db1c77968913d
Valerii Strilets <valerii.strilets@gmail.com>
2023-10-28 17:04:24 +0300

    update docs

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,38 +1,31 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'Vue Kinesis',
+    description: 'Easily create complex interactive animations with Vue.js.',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      github: 'letstri/vue-kinesis',
     },
     github: {
       dir: '.starters/default/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      repo: 'vue-kenesis',
+      owner: 'letstri',
+      edit: true,
     },
     aside: {
       level: 0,
       collapsed: false,
-      exclude: []
+      exclude: [],
     },
     main: {
       padded: true,
-      fluid: true
+      fluid: true,
     },
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
-    }
-  }
-})
+      fluid: true,
+    },
+  },
+});
