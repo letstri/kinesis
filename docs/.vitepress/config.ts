c0efba3d8d37aa1152a3332a717f4c3afae38d1d
Valerii Strilets <valerii.strilets@gmail.com>
2023-11-13 22:38:18 +0200

    update description

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Vue Kinesis",
-  description: "Easily create complexe interactive animations with Vue 3",
+  title: 'Vue Kinesis',
+  description: 'Easy creation of complex interactive animation using Vue 3',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +16,11 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+    socialLinks: [{ icon: 'github', link: 'https://github.com/letstri/vue0-kinesis' }],
+  },
+});
