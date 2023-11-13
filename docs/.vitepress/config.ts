8361f262ec59b0c1ddfa248bf286c397b7ebfcb9
Valerii Strilets <valerii.strilets@gmail.com>
2023-11-13 21:34:55 +0200

    remove index.html

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
     ],
-
     sidebar: [
       {
         text: 'Examples',
@@ -20,7 +19,6 @@ export default defineConfig({
         ],
       },
     ],
-
     socialLinks: [{ icon: 'github', link: 'https://github.com/letstri/vue-kinesis' }],
   },
 });
